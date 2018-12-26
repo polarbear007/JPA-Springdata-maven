a54a9a945e4267eea2811e8cb0c747157d310840
eric <1479124685@qq.com>
2018-12-26 14:05:07 +0800

    通过 SpringTest3.java 演示使用springdata 关键字来声明dao 方法

--- src/main/java/cn/itcast/jpa/springdata/dao/EmployeesDao.java
+++ src/main/java/cn/itcast/jpa/springdata/dao/EmployeesDao.java
@@ -3,14 +3,31 @@ package cn.itcast.jpa.springdata.dao;
 import java.util.Date;
 import java.util.List;
 
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import cn.itcast.jpa.springdata.entity.Employees;
 import cn.itcast.jpa.springdata.entity.Employees.GenderType;
 
-public interface EmployeesDao extends PagingAndSortingRepository<Employees, Integer> {
-	Employees findByEmpNo(Integer empNo);
-	Employees getByLastNameAndFirstName(String lastName, String firstName);
+public interface EmployeesDao extends JpaRepository<Employees, Integer>{
+	// 通过lastName 和 firstName 查询(有可能出现同名的，如果你只用一个 Employees 去接，可能会报错)
+	List<Employees> getByLastNameAndFirstName(String lastName, String firstName);
+	
+	// 查询出生日期早于某个指定时间的记录（比如查在2000年以前出生的记录）
 	List<Employees> getByBirthDateBefore(Date date);
+	
+	// 查询出生日期早于某个指定时间的记录（比如查在2000年以前出生的记录）----> 条件带分页查询
+	Page<Employees> getByBirthDateBefore(Date date, Pageable pageable);
+	
+	// 查询出生日期晚于某个时间，而且性别等于某个类型的记录（比如查在2000年以后出生的、性别为女的记录）
 	List<Employees> getByBirthDateAfterAndGender(Date date, GenderType gender);
+	
+	// 查询 lastName 以 Ac 开头的
+	List<Employees> getByLastNameStartingWith(String lastName);
+	
+	// 查询 lastName 以 d 结尾的
+	List<Employees> getByLastNameEndingWith(String lastName);
+	
+	
 }
--- src/test/java/cn/itcast/jpa/springdata/test/SpringdataTest3.java
+++ src/test/java/cn/itcast/jpa/springdata/test/SpringdataTest3.java
@@ -1,17 +1,97 @@
 package cn.itcast.jpa.springdata.test;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import cn.itcast.jpa.springdata.service.EmployeesService;
+import cn.itcast.jpa.springdata.dao.EmployeesDao;
+import cn.itcast.jpa.springdata.entity.Employees;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:applicationContext.xml")
 public class SpringdataTest3 {
+	// 同样，因为只是要测试一下 dao 层的方法，根本就不想通过 service 层调用，
+	// 所以这里直接注入 dao 层的代理对象，然后直接测试方法
 	@Autowired
-	private EmployeesService employeesService;
+	private EmployeesDao employeesDao;
 	
+	@Test
+	public void test1() {
+		List<Employees> list = employeesDao.getByLastNameAndFirstName("Simmel", "Bezalel");
+		if(list != null && list.size() > 0) {
+			for (Employees employees : list) {
+				System.out.println(employees);
+			}
+		}
+	}
 	
+	@Test
+	public void test2() throws ParseException {
+		String str = "1955-3-1";
+		Date date = new SimpleDateFormat("yyyy-MM-dd").parse(str);
+		List<Employees> list = employeesDao.getByBirthDateBefore(date);
+		if(list != null && list.size() > 0) {
+			for (Employees employees : list) {
+				System.out.println(employees);
+			}
+		}
+	}
+	
+	//【注意】 通过关键字带声明的 dao 方法，还可以额外加上排序或者分页的条件！！！ 也就是加上 Sort 或者  PageRequest 对象。
+	@Test
+	public void test3() throws ParseException {
+		String str = "1955-3-1";
+		Date date = new SimpleDateFormat("yyyy-MM-dd").parse(str);
+		Pageable pageable = new PageRequest(0, 10);
+		Page<Employees> page = employeesDao.getByBirthDateBefore(date, pageable);
+		
+		if(page.hasContent()) {
+			List<Employees> list = page.getContent();
+			for (Employees employees : list) {
+				System.out.println(employees);
+			}
+		}
+	}
+	
+	@Test
+	public void test4() throws ParseException {
+		String str = "1955-3-1";
+		Date date = new SimpleDateFormat("yyyy-MM-dd").parse(str);
+		List<Employees> list = employeesDao.getByBirthDateAfterAndGender(date, Employees.GenderType.F);
+		if(list != null && list.size() > 0) {
+			for (Employees employees : list) {
+				System.out.println(employees);
+			}
+		}
+	}
+	
+	@Test
+	public void test5() {
+		List<Employees> list = employeesDao.getByLastNameStartingWith("ad");
+		if(list != null && list.size() > 0) {
+			for (Employees employees : list) {
+				System.out.println(employees);
+			}
+		}
+	}
+	
+	@Test
+	public void  test6() {
+		List<Employees> list = employeesDao.getByLastNameEndingWith("s");
+		if(list != null && list.size() > 0) {
+			for (Employees employees : list) {
+				System.out.println(employees);
+			}
+		}
+	}
 }
